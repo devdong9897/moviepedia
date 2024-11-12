f8f6e12b0b951c613a0d5941f16ff02fb1d571d1
devdong9897 <dev.dong9897@gmail.com>
2024-11-12 15:32:43 +0900

    하나의 state로 폼 구현

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -2,36 +2,37 @@ import { useState } from "react";
 import "./ReviewForm.css";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // 여기서 prevValue를 사용하면 title, rating, content중 변경된 필드만 갱신되고 나머지는 유지되게 한다.
+    // 예를 들어, title을 "Great Movie!"로 변경하면 [name]: value 이 부분은 title: "Great Movie!" 실질적으로 이렇게 되고,
+    // values는 { title: "Great Movie!", rating: 0, content: "" }로 설정된다.
+    setValues((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({
-      title,
-      rating,
-      content,
-    });
+    console.log(values);
   };
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input
+        name="rating"
+        type="number"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea name="content" value={values.content} onChange={handleChange} />
       <button type="submit">확인</button>
     </form>
   );
